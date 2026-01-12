43cc9094d267dd83daf367c2544c5f597756bfbd
alexdolin <moroz9tina@gmail.com>
2026-01-12 20:11:53 +0500

    test commit-4

--- src/main/java/dolin/dev/demogit/UserService.java
+++ src/main/java/dolin/dev/demogit/UserService.java
@@ -4,4 +4,7 @@ public class UserService {
     public void getUser(){
         System.out.println("Hello I`m user");
     }
+    public void setUser(){
+        System.out.println("Вы изменили юзера");
+    }
 }
